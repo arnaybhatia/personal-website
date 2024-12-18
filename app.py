f18b2fba4183d57270e662037945fcd9f5ad706d
strifedeeno <coolarnay@gmail.com>
2024-12-18 00:03:05 -0800

    Update Flask server to use environment variable for port configuration

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='static')
@@ -11,4 +12,5 @@ def serve_static(path):
     return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port)
